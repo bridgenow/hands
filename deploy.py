20f373f763ab2051f0f154fad90f50c94ecd371f
Mark Dessain <mark.dessain@gmail.com>
2017-04-23 20:35:58 +0100

    setup user

--- deploy.py
+++ deploy.py
@@ -9,12 +9,18 @@ log = logging.getLogger(__name__)
 
 def main():
     commit = os.environ.get("TRAVIS_COMMIT", "manual")
+    author_email = os.environ.get("COMMIT_AUTHOR_EMAIL")
+    author_name = os.environ.get("COMMIT_AUTHOR_NAME")
 
     shutil.copytree('../home/.git', '../template/deploy/.git')
 
     log.info('Commiting ...')
     subprocess.check_call(['git', 'add', '-A'], cwd='../template/deploy')
     subprocess.check_call(['git', 'commit', '-a', '-m', 'Site deploy for %s' % commit], cwd='../template/deploy')
+
+    log.info('Git user ...')
+    subprocess.check_call(['git', 'config', 'user.email', author_email], cwd='../template/deploy')
+    subprocess.check_call(['git', 'config', 'user.name', author_name], cwd='../template/deploy')
 
     log.info('Pushing ...')
     subprocess.check_call(['git', 'push'], cwd='../template/deploy')
